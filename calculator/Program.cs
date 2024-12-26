01792b1f61c44b9c12bb62e1f59338fde79aa029
zombiman <zombiman@DESKTOP-0BVK8U0>
2024-12-27 02:21:00 +0300

    Добавьте файлы проекта.

---
+++ calculator/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace calculator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("INPUTE COMMAND : ");
+            string calculcstring;
+
+            calculcstring = Console.ReadLine(); Console.WriteLine("\n");
+            string[] result = calculcstring.Split(' ');
+    
+            float res = 0;
+            switch (result[1]) {
+                case "+":
+                     res = float.Parse(result[0]) + float.Parse(result[2]);
+                    Console.WriteLine("Result : " + res);
+                    break;
+                case "-":
+                     res = float.Parse(result[0]) - float.Parse(result[2]);
+                    Console.WriteLine("Result : " + res);
+                    break;
+                case "*":
+                     res = float.Parse(result[0]) * float.Parse(result[2]);
+                    Console.WriteLine("Result : " + res);
+                    break;
+                case "/":
+                     res = float.Parse(result[0]) / float.Parse(result[2]);
+                    Console.WriteLine("Result : " + res);
+                    break;
+                default:
+                    Console.WriteLine("Wrong value!!!!");
+                    break;
+
+            }
+           
+        }
+    }
+}
---
+++ firstnamelastname/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace firstnamelastname
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("what is your name?");
+
+            string name = Console.ReadLine();
+            Console.WriteLine("Hello "+ name +"!");
+        }
+    }
+}
